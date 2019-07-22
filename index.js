91bf13a0a255b4e74851560efb237b2a3f71cbde
Winnie <winniebosy@gmail.com>
2019-07-22 14:59:38 +0300

    sampled the forEach loop

--- index.js
+++ index.js
@@ -5,6 +5,43 @@ alert(colors[0]);
 
 //another instance of creating an array
 var countries = new Array('Kenya', 'Uganda', 'Tanzania');
+    countries.push('Rwanda');
     
-    countries.pull([0]);
     alert(countries);
+
+
+
+
+    //LOOPS
+
+    var x = [2,3,4,5,6,7];
+    x.forEach(function(x){
+        console.table(x);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
