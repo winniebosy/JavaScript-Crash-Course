d9a8eb6f44a0101c8b2d558ac4566ce0f4a2295b
Winnie <winniebosy@gmail.com>
2019-07-22 12:41:40 +0300

    made an attempt on array.push for Rwanda

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // declaring arrays
 var colors =['red', 'blue', 'green'];
+
 alert(colors[0]);
+
+//another instance of creating an array
+var countries = new Array('Kenya', 'Uganda', 'Tanzania');
+    
+    countries.pull([0]);
+    alert(countries);
